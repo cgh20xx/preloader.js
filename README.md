b826da70f53f85856b0dfd78402578a83796c90e
Hank-mbp <cgh20xx@gmail.com>
2018-09-11 17:00:16 +0800

    新支援 mp3 mp4 格式

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # preloader
+提供預載圖片、聲音、影片功能
+支援格式包含 jpg png mp3 mp4
+
+# Download
+[preloader.js](https://cgh20xx.github.io/preloader/preloader.js)
+
+# Example
+```javascript
+var cache; // to save loaded sources
+
+preloader({
+    manifest : [
+        {id: 'img_1', src: 'images/img1.png'}, // 圖片預設可省略 type: 'image' 
+        {id: 'img_2', src: 'images/img2.png'},
+        {id: 'img_3', type: 'image', src: 'images/img3.png'},
+        {id: 'audio', type: 'audio', src: 'audio/ado.mp3'},
+        {id: 'video', type: 'video', src: 'video/vdo.mp4'},
+    ],
+    onEachLoad: function(info) {
+        console.log('讀取中(' + info.index + '/' + info.total + ')');
+    },
+    onAllLoad: function(source) {
+        console.log('preload all completed');
+        cache = source;
+    }
+});
+```
--- preloader.js
+++ preloader.js
@@ -2,9 +2,9 @@
  *  preloader.js
  *
  *  Author : Hank Hsiao
- *  Version: 1.0.0
+ *  Version: 1.1.0
  *  Create : 2015.8.18
- *  Update : 2016.05.24
+ *  Update : 2018.09.11
  *  License: MIT
  */
 
@@ -12,40 +12,97 @@ var preloader = function(option) {
     var queue = {}; //儲存載完的檔案
     var settings = {
         manifest: [],
-        onEachLoad: function(info) { console.log('[imagePreloader] image:' + info.index + ' loaded');},
-        onAllLoad: function(queue) { console.log('[imagePreloader] all images loaded:' + queue);}
+        onEachLoad: function(info) { console.log('[Preloader] ' + info.index + ' loaded');},
+        onAllLoad: function(queue) { console.log('[Preloader] all completed:' + queue);}
     };
 
-    // 模擬 option extend settings (淺拷貝)
-    var k;
-    for (k in settings) {
-        if (option[k]) { settings[k] = option[k]; }
+    // option extend settings (淺拷貝)
+    var key;
+    for (key in settings) {
+        if (option[key]) { settings[key] = option[key]; }
     }
 
-    var imgQty = settings.manifest.length; //圖片數量(未載入)
-    var loadedImgQty = 0; //已經載入完成的圖片數量
-    var img;
-    var i;
+    var allQty = settings.manifest.length; // 資源數量(未載入)
+    var loadedQty = 0; // 已經載入完成的資源數量
 
-    for (i = 0; i < imgQty; i += 1) {
-        img = new Image();
-        console.log(img.complete);
-        img.addEventListener('load', (function(index, image, id) {
-            return function(e) {
-                loadedImgQty++;
-                var imgInfo = {
-                    index: loadedImgQty,
-                    image: image,
-                    imgQty: imgQty
+    function handleLoadEvent(data) {
+        settings.onEachLoad(data);
+        queue[data.id] = data;
+        if (loadedQty === allQty) {
+            settings.onAllLoad(queue);
+        }
+    }
+
+    var handler = {
+        image: function(id, e) {
+            loadedQty++;
+            var data = {
+                id: id,
+                index: loadedQty,
+                total: allQty,
+                img: this
+            };
+            handleLoadEvent(data)
+        },
+        audio: function(id, e) {
+            if (e.target.status === 200) {
+                loadedQty++;
+                var data = {
+                    id: id,
+                    index: loadedQty,
+                    total: allQty,
+                    audio: this
                 };
-                settings.onEachLoad(imgInfo);
-                queue[id] = imgInfo;
-                //如果已載入的圖片數量等於圖片總數，表全部載入完成
-                if (loadedImgQty == imgQty) {
-                    settings.onAllLoad(queue);
-                }
+                this.src = URL.createObjectURL(e.target.response);
+                handleLoadEvent(data)
+            } else {
+                onError(id);
             }
-        })(i, img, settings.manifest[i].id), false);
-        img.src = settings.manifest[i].src;
+        },
+        video: function(id, e) {
+            if (e.target.status === 200) {
+                loadedQty++;
+                var data = {
+                    id: id,
+                    index: loadedQty,
+                    total: allQty,
+                    video: this
+                };
+                this.src = URL.createObjectURL(e.target.response);
+                handleLoadEvent(data)
+            } else {
+                onError(id);
+            }
+        },
+    };
+
+    function onError(id) {
+        console.error('[Preloader] not found : ' + id);
     }
-}
+
+    for (var i = 0; i < allQty; i += 1) {
+        var preloadObj = settings.manifest[i];
+        if (preloadObj.type === 'audio') {
+            var audio = document.createElement('audio');
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', preloadObj.src, true);
+            xhr.responseType = 'blob';
+            xhr.addEventListener('load', handler.audio.bind(audio, preloadObj.id), false);
+            xhr.addEventListener('error', onError.bind(audio, preloadObj.id), false);
+            xhr.send();
+        } else if (preloadObj.type === 'video') {
+            var video = document.createElement('video');
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', preloadObj.src, true);
+            xhr.responseType = 'blob';
+            xhr.addEventListener('load', handler.video.bind(video, preloadObj.id), false);
+            xhr.addEventListener('error', onError.bind(video, preloadObj.id), false);
+            xhr.send();
+        } else {
+            var img = document.createElement('img');
+            img.addEventListener('load', handler.image.bind(img, preloadObj.id), false);
+            img.addEventListener('error', onError.bind(img, preloadObj.id), false);
+            img.src = preloadObj.src;
+        }
+    }
+};
